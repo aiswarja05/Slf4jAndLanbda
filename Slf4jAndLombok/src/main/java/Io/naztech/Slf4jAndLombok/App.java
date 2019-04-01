b105d7efb450acd5f129e7da1e1af07cbc1a4892
aiswarja05 <aiswarja_saha05@yahoo.example.com>
2019-04-01 16:29:45 +0600

    Slf4jAndLombok

---
+++ Slf4jAndLombok/src/main/java/Io/naztech/Slf4jAndLombok/App.java
@@ -0,0 +1,13 @@
+package Io.naztech.Slf4jAndLombok;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
